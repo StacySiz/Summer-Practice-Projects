12ec663e82cda387a952e2e63bbdd56fa8e65ac7
StacySiz <nastyasizova@gmail.com>
2018-07-12 19:17:13 +0300

    Get user by id option

--- WeatherAppNodeJS/app/controller/userController.js
+++ WeatherAppNodeJS/app/controller/userController.js
@@ -6,6 +6,10 @@ var config = require('../config/env');
 const jwtInfo = require('../config/jwtConf');
 const express = require('express');
 const app = express();
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/weather_app';
+const client = new pg.Client(connectionString);
+client.connect();
 
 exports.create = (req, res) => {
     const hashedPassword = bcrypt.hashSync(req.body.password,8);
@@ -24,21 +28,28 @@ exports.create = (req, res) => {
 };
 
 exports.findOne = (req,res) => {
-    User.findOne({where: {email: req.body.email}},function (err,user) {
-        console.log(req.body.email);
-        if (err) return res.status(500).send('Error on server');
-        if (!user) return res.status(404).send('User not found');
+    var tag = req.query.q;
+    res.json(tag)
+    var search = client.query('select * from users where email Ilike $1', [`%${req.query.q}%`], function(err, result){
+            res.json(result);
+            console.log(res.json(result));
+        }
+    );
 
-       const validPassword = bcrypt.compareSync(req.body.password,user.password);
-       if (!validPassword) return res.status(401).send({ auth: false, token: null });
-        //if (req.body.password != user.password) return res.status(401).send({auth:false,token:null});
-
-        const token = jwt.sign({id: user.id},jwtInfo.JWT_SECRET,{expiresIn:jwtInfo.tokenLife});
-        res.status(200).send({ auth: true, token: token });
-
-        res.render('index',{weather: null, error: null,user:token.firstname});
-
-    })
+    // User.findOne({where: {email: req.body.email}},function (err,user) {
+    //     console.log(req.body.email);
+    //     if (err) return res.status(500).send('Error on server');
+    //     if (!user) return res.status(404).send('User not found');
+    //     console.log("step 1");
+    //    const validPassword = bcrypt.compareSync(req.body.password,user.password);
+    //    if (!validPassword) return res.status(401).send({ auth: false, token: null });
+    //
+    //     const token = jwt.sign({id: user.id},jwtInfo.JWT_SECRET,{expiresIn:jwtInfo.tokenLife});
+    //     res.status(200).send({ auth: true, token: token });
+    //
+    //    // res.render('index',{weather: null, error: null,user:token.firstname});
+    //
+    // })
         //.then(token =>{
        // res.render('index',{weather: null, error: null,user:token.firstname});
         //res.status(200).send({ auth: true, token: token });
--- WeatherAppNodeJS/app/route/userRoute.js
+++ WeatherAppNodeJS/app/route/userRoute.js
@@ -1,3 +1,7 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/weather_app';
+const client = new pg.Client(connectionString);
+client.connect();
 module.exports = function(app) {
 
     const users = require('../controller/userController.js');
@@ -16,7 +20,31 @@ module.exports = function(app) {
         res.render('signIn');
     });
 
-    app.post('/signIn',users.findOne);
+    // app.post('/signIn',users.findOne);
+
+    router.post('signIn', (req, res, next) => {
+        const results = [];
+        // Get a Postgres client from the connection pool
+        pg.connect(connectionString, (err, client, done) => {
+            // Handle connection errors
+            if(err) {
+                done();
+                console.log(err);
+                return res.status(500).json({success: false, data: err});
+            }
+            // SQL Query > Select Data
+            const query = client.query('select * from users where email Ilike $1\', [`%${req.query.q}%`]');
+            // Stream results back one row at a time
+            query.on('row', (row) => {
+                results.push(row);
+            });
+            // After all data is returned, close connection and return results
+            query.on('end', () => {
+                done();
+                return res.json(results);
+            });
+        });
+    });
 
     // Retrieve all Customer
     app.get('/users', users.findAll);
